7f41369e81e1654c83013f2d4577953f20551df8
rhkzleek <978415719@qq.com>
2018-07-06 17:02:29 +0800

    Revert "优化"
    This reverts commit 64214ca46c06da16ac8afaed146c029cfee3a9c4.

--- python3_web/www/coroweb.py
+++ python3_web/www/coroweb.py
@@ -39,8 +39,8 @@ def post(path):
 
 def get_required_kw_args(fn):
     args = []
-    params = inspect.signature(fn)
-    for name,param in params.item():
+    params = inspect.signature(fn).parameters
+    for name,param in params.items():
         if param.kind == inspect.Parameter.KEYWORD_ONLY and param.default == inspect.Parameter.empty:
             args.append(name)
     return tuple(args)
